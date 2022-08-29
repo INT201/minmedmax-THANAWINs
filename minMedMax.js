57296de7e6485b393f47062bf070edd46de333f5
MILLOR <89179883+THANAWINs@users.noreply.github.com>
2022-08-29 19:57:53 +0700

    submit(2)

--- minMedMax.js
+++ minMedMax.js
@@ -3,7 +3,7 @@ const { template } = require('@babel/core')
 function minMedMax(n1, n2, n3) {
     //code here
     if(n1>n2 && n1>n3){
-        if(n2>n3){ //max = n1  //321
+        if(n2>n3){ //max = n1 
             return { min: n3, mid: n2, max: n1 }
         }else{
             return { min: n2, mid: n3, max: n1 }
